b878c6155a5e1a0867e16e5b0081e505d3db04ed
icemk <ickaymakci@gmail.com>
2025-03-16 10:08:27 +0400

    Update app4.py

--- app4.py
+++ app4.py
@@ -42,8 +42,8 @@ from supabase import create_client, Client
 import uuid
 
 # These must be stored in your Streamlit secrets under the [SUPABASE] section
-SUPABASE_URL = #st.secrets["SUPABASE"]["URL"]
-SUPABASE_KEY = #st.secrets["SUPABASE"]["KEY"]
+SUPABASE_URL = st.secrets["SUPABASE"]["URL"]
+SUPABASE_KEY = st.secrets["SUPABASE"]["KEY"]
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
